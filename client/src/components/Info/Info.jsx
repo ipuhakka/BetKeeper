d4916432caea9eae4788c29b2b4dbdf9b6ec3a21
Iiro Puhakka <iiropuhakka@luukku.com>
2019-04-14 17:52:11 +0300

    Add fade-in-out animation to Info

--- client/src/components/Info/Info.jsx
+++ client/src/components/Info/Info.jsx
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {clearAlert} from '../../actions/alertActions';
 import Alert from 'react-bootstrap/lib/Alert';
 import Button from 'react-bootstrap/lib/Button';
+import './Info.css';
+
+const TIME_VISIBLE = 4000;
 
 class Info extends Component{
 
 	render(){
-		return(<div>{this.renderAlert()}</div>);
-	}
 
-	//This function uses a switch to return a type of alert or null. Switch is prop 'alertState'. Its values are statuscodes of
-	//http-requests. Other alert types use code -1. Any other value returns no alert element.
-	renderAlert = () => {
 		let style = "success";
 		if (this.props.status >= 400 || this.props.status === -1){
 			style = "warning";
@@ -23,34 +20,21 @@ class Info extends Component{
 		}
 
 		if (this.props.status !== null){
-			if (this.props.timeout > 0){
-				setTimeout(() => {
-					this.dismiss();
-				}, this.props.timeout);
-			}
+			setTimeout(() => {
+				this.dismiss();
+			}, TIME_VISIBLE);
+		}
 
-			return(<Alert bsStyle={style} onDismiss={this.dismiss}>
+		return(<Alert className={this.props.status !== null ? 'visible' : 'hidden'} bsStyle={style} onDismiss={this.dismiss}>
 					<p>{this.props.statusMessage}</p>
 					<Button onClick={this.dismiss}>{"Hide"}</Button>
-					</Alert>);
-		} else {
-			return null;
-		}
+				</Alert>);
 	}
 
 	dismiss = () => {
 		this.props.clearAlert();
 	}
-
 }
-
-Info.defaultProps = {
-	timeout: 2000 //number of milliseconds alert is visible. If set to 0 or less, alert is closed only manually
-};
-
-Info.propTypes = {
-	timeout: PropTypes.number
-};
 
 const mapStateToProps = (state, ownProps) => {
   return { ...state.alert}
