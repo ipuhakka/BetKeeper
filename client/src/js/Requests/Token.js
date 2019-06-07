9bf0a4e879dcec9ba06a6cfde01353a6bc34b606
Iiro Puhakka <iiropuhakka@luukku.com>
2019-06-07 16:06:58 +0300

    Implements private routing

--- client/src/js/Requests/Token.js
+++ client/src/js/Requests/Token.js
@@ -11,7 +11,7 @@ data:{
 On success, resolved with parsed token, user-id and username as parameters.
 Rejects with status of received http-response.
 */
-export function postToken(username, password, callback){
+export function postToken(username, password){
   return new Promise(function(resolve, reject){
     var xmlHttp = new XMLHttpRequest();
 
