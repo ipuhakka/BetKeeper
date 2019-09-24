a4f6d856c15d1948e2cdbc5aeaf8c4cd6081fbe1
ipuhakka <iiropuhakka@luukku.com>
2019-09-24 21:15:21 +0300

    Adds Http-helper class

---
+++ Betkeeper/Betkeeper/src/Data/Http.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Betkeeper.Data
+{
+    public class Http
+    {
+
+        /// <summary>
+        /// Creates a HttpResponseMessage.
+        /// </summary>
+        /// <param name="httpStatusCode"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static HttpResponseMessage CreateResponse(HttpStatusCode httpStatusCode, object data = null)
+        {
+            // TODO: Yksikkötestit
+            var response = new HttpResponseMessage(httpStatusCode)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(data))
+            };
+
+            return response;
+        }
+
+        /// <summary>
+        /// Returns request body as dynamic.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static dynamic GetRequestBody(HttpRequestMessage request)
+        {
+            // TODO: Yksikkötestit
+            HttpContent requestContent = request.Content;
+            string jsonContent = requestContent.ReadAsStringAsync().Result;
+
+            return (dynamic)JsonConvert.DeserializeObject(jsonContent);
+        }
+    }
+}
