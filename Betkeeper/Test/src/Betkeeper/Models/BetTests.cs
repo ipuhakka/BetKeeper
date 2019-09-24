24bd1a532724e423e138cd666c29dbd9af76d3a5
ipuhakka <iiropuhakka@luukku.com>
2019-09-24 21:19:41 +0300

    Removes unnecessary usings

--- Betkeeper/Test/src/Betkeeper/Models/BetTests.cs
+++ Betkeeper/Test/src/Betkeeper/Models/BetTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Betkeeper;
 using Betkeeper.Models;
 using Betkeeper.Exceptions;
