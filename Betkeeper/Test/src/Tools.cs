7b0e0c143fe3c74b6a6855d6961d7c6eb97a9339
ipuhakka <iiropuhakka@luukku.com>
2019-09-26 19:15:04 +0300

    Add MockControllerContext-method

--- Betkeeper/Test/src/Tools.cs
+++ Betkeeper/Test/src/Tools.cs
@@ -2,8 +2,11 @@
 using System.Data.SQLite;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
+using System.Web.Http.Controllers;
 using Betkeeper;
 using NUnit.Framework;
+using Newtonsoft.Json;
 
 namespace Test
 {
@@ -74,6 +77,26 @@ namespace Test
             GC.WaitForPendingFinalizers();
 
             File.Delete(testDatabaseName);
+        }
+
+        public static HttpControllerContext MockHttpControllerContext(object dataContent = null, Dictionary<string, string> headers = null)
+        {
+            var request = new HttpRequestMessage();
+
+            request.Content = new StringContent(JsonConvert.SerializeObject(dataContent));
+
+            if (headers != null)
+            {
+                foreach (var headerDictRow in headers)
+                {
+                    request.Headers.Add(headerDictRow.Key, headerDictRow.Value);
+                }
+            }
+
+            return new HttpControllerContext
+            {
+                Request = request
+            };
         }
     }
 }
