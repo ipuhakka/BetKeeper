2b3aaa8e54c1d123b3ca997055a121e7c2eefea3
ipuhakka <iiropuhakka@luukku.com>
2019-11-24 14:54:04 +0200

    Adds overflow handling to menu to keep it in one line

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -6,29 +6,128 @@ import Nav from 'react-bootstrap/Nav';
 import PropTypes from 'prop-types';
 import Spinner from '../Spinner/Spinner.jsx';
 import './Menu.css';
+import _ from 'lodash';
 
 class Menu extends Component{
+	constructor(props)
+	{
+		super(props);
+
+		this.menuRef = React.createRef();
+
+		this.state = {
+			scrollLeft: false,
+			scrollRight: false
+		}
+	}
+
+	componentDidMount()
+	{
+		window.addEventListener('resize', this.handleOverflow);
+		this.handleOverflow();
+	}
+
 	render(){
+		const {scrollLeft, scrollRight} = this.state;
 
 		return(
-			<Nav variant="tabs" onSelect={this.handleSelect} as="ul">
-				<Nav.Item as="li">
-					<Nav.Link eventKey={0} disabled={this.props.disable[0]}>Home</Nav.Link>
-				</Nav.Item>
-				<Nav.Item as="li">
-					<Nav.Link eventKey={1} disabled={this.props.disable[1]}>Bets</Nav.Link>
-				</Nav.Item>
-				<Nav.Item as="li">
-					<Nav.Link eventKey={2} disabled={this.props.disable[2]}>Statistics</Nav.Link>
-				</Nav.Item>
-				<Nav.Item as="li">
-					<Nav.Link eventKey={3} disabled={this.props.disable[3]}>Folders</Nav.Link>
-				</Nav.Item>
-				<Nav.Item as="li">
-					<Nav.Link eventKey={4} disabled={this.props.disable[4]}>Logout</Nav.Link>
-				</Nav.Item>
-				<Spinner as="li" className="spinner" active={this.props.loading}/>
-			</Nav>);
+			<div ref={this.menuRef} className='menu-div'>
+				<button
+					onClick={() => this.scrollTo('left')} 
+					className={`scroll-button left${scrollLeft
+					? ''
+					: ' hidden'}`}>{'<'}</button>
+				<Nav variant="tabs" onSelect={this.handleSelect} as="ul">
+					<Nav.Item as="li">
+						<Nav.Link eventKey={0} disabled={this.props.disable[0]}>Home</Nav.Link>
+					</Nav.Item>
+					<Nav.Item as="li">
+						<Nav.Link eventKey={1} disabled={this.props.disable[1]}>Bets</Nav.Link>
+					</Nav.Item>
+					<Nav.Item as="li">
+						<Nav.Link eventKey={2} disabled={this.props.disable[2]}>Statistics</Nav.Link>
+					</Nav.Item>
+					<Nav.Item as="li">
+						<Nav.Link eventKey={3} disabled={this.props.disable[3]}>Folders</Nav.Link>
+					</Nav.Item>
+					<Nav.Item as="li">
+						<Nav.Link eventKey={4} disabled={this.props.disable[4]}>Logout</Nav.Link>
+					</Nav.Item>
+					<Spinner as="li" className="spinner" active={this.props.loading}/>
+				</Nav>
+				<button
+					onClick={() => this.scrollTo('right')}  
+					className={`scroll-button right${scrollRight
+					? ''
+					: ' hidden'}`}>{'>'}</button>
+			</div>);
+	}
+
+	/**
+	 * Scrolls menu to given direction.
+	 * @param {string} direction  
+	 */
+	scrollTo(direction)
+	{
+		const menu  = this.menuRef.current;
+
+		if (_.isNil(menu))
+		{
+			return;
+		}
+
+		menu.scrollTo({
+			left: direction === 'left'
+				? 0
+				: menu.scrollWidth,
+			top: 0,
+			behavior: 'smooth'
+		});
+
+		this.setState({
+			scrollLeft: !this.state.scrollLeft,
+			scrollRight: !this.state.scrollRight
+		});
+	}
+
+	/**
+	 * Handles menu overflow.
+	 */
+	handleOverflow = () => {
+		if (_.isNil(this.menuRef.current))
+		{
+			return;
+		}
+
+		const { scrollWidth, clientWidth, scrollLeft } = this.menuRef.current;
+
+		const overflows = scrollWidth - clientWidth;
+
+		if (overflows)
+		{
+			if (scrollLeft === 0)
+			{
+				this.setState(
+				{
+					scrollRight: true,
+					scrollLeft: false
+				});
+			}
+			else 
+			{
+				this.setState({
+					scrollLeft: true,
+					scrollRight: false
+				});
+			}
+		}
+		else if (this.state.scrollLeft || this.state.scrollRight)
+		{
+			this.setState({
+				scrollLeft: false,
+				scrollRight: false
+			});
+		}
 	}
 
 	handleSelect = async (key) => {
