c4fc29d0af4e1b271bd5de057740071aeb17661c
ipuhakka <iiropuhakka@luukku.com>
2020-02-09 12:09:34 +0200

    Implement generalized random string generation

---
+++ Betkeeper/Betkeeper/src/Classes/StringUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Betkeeper.Classes
+{
+    public class StringUtils
+    {
+        /// <summary>
+        /// Generates a random string of specified length.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string GenerateRandomString(int length)
+        {
+            const string validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+            StringBuilder randomString = new StringBuilder();
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] uintBuffer = new byte[sizeof(uint)];
+
+                for (int i = 0; i < length; i++)
+                {
+                    rng.GetBytes(uintBuffer);
+                    uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                    randomString.Append(validCharacters[(int)(num % (uint)validCharacters.Length)]);
+                }
+            }
+
+            return randomString.ToString();
+        }
+    }
+}
---
+++ Betkeeper/Test/src/Betkeeper/Classes/StringUtilsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Betkeeper.Classes;
+using NUnit.Framework;
+
+namespace Betkeeper.Test.Classes
+{
+    [TestFixture]
+    public class StringUtilsTests
+    {
+        [Test]
+        public void GenerateRandomString_ReturnsStringOfSpecifiedLength()
+        {
+            new List<int>
+            {
+                15,
+                12,
+                10
+            }.ForEach(length =>
+            {
+                var randomString = StringUtils.GenerateRandomString(length);
+
+                Assert.AreEqual(length, randomString.Length);
+            });
+        }
+    }
+}
