7739b84d8fba4bfd0084f475a0a0f4afbb66a162
ipuhakka <iiropuhakka@luukku.com>
2020-06-23 18:17:44 +0300

    Creates IsNullOrWhiteSpace extension for JToken

---
+++ Betkeeper/Betkeeper/src/Extensions/JToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace Betkeeper.Extensions
+{
+    public static class JTokenExtensions
+    {
+        /// <summary>
+        /// Checks if JToken is null or whitespace.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static bool IsNullOrWhiteSpace(this JToken token)
+        {
+            return string.IsNullOrWhiteSpace(token?.ToString());
+        }
+    }
+}
---
+++ Betkeeper/Test/src/Betkeeper/Extensions/JTokenExtensionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Betkeeper.Extensions;
+
+namespace Test.src.Betkeeper.Extensions
+{
+    [TestFixture]
+    public class JTokenExtensionTests
+    {
+        [Test]
+        public void IsNullOrWhiteSpace_WhiteSpaceReturnsTrue()
+        {
+            Assert.IsTrue(new JValue("  ").IsNullOrWhiteSpace());
+        }
+
+        [Test]
+        public void IsNullOrWhiteSpace_EmptyReturnsTrue()
+        {
+            Assert.IsTrue(new JValue("").IsNullOrWhiteSpace());
+        }
+
+        [Test]
+        public void IsNullOrWhiteSpace_NullReturnsTrue()
+        {
+            Assert.IsTrue(new JValue((string)null).IsNullOrWhiteSpace());
+        }
+
+        [Test]
+        public void IsNullOrWhiteSpace_ContainsValueReturnsTrue()
+        {
+            Assert.IsFalse(new JValue(true).IsNullOrWhiteSpace());
+            Assert.IsFalse(new JValue(1).IsNullOrWhiteSpace());
+            Assert.IsFalse(new JValue("Not empty").IsNullOrWhiteSpace());
+        }
+    }
+}
