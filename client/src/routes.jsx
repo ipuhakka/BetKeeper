988242c1107aece9a76879fa188b019f81c974db
ipuhakka <iiropuhakka@luukku.com>
2021-01-23 13:12:02 +0200

    Remove old Home page related code

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch} from 'react-router-dom';
 import LoginView from './views/LoginView/LoginView';
-import Home from './views/Home/Home';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import PageContainer from './containers/PageContainer';
 
@@ -10,7 +9,6 @@ const Routes = () => {
     <Router>
       <Switch>
         <Route exact path='/' component={LoginView}/>
-        <PrivateRoute path='/home' component={Home} />
         <PrivateRoute path='/page' component={PageContainer} />
       </Switch>
     </Router>
