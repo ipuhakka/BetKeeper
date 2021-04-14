9a84f17287830dccb02aefc763b4f054abccfdeb
ipuhakka <iiropuhakka@luukku.com>
2021-04-14 17:15:49 +0300

    Add competition invitation models

---
+++ Betkeeper/BetkeeperCore/src/Actions/CompetitionInvitationAction.cs
@@ -0,0 +1,97 @@
+﻿using Betkeeper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betkeeper.Actions
+{
+    public class CompetitionInvitationAction
+    {
+        /// <summary>
+        /// Invite users to competition
+        /// </summary>
+        /// <param name="competitionId"></param>
+        /// <param name="hostUserId"></param>
+        /// <param name="userIds"></param>
+        public void InviteUsers(int competitionId, int hostUserId, List<string> usernames)
+        {
+            var competition = new CompetitionAction().GetCompetition(competitionId);
+
+            if (competition == null)
+            {
+                throw new ActionException(Enums.ActionResultType.NotFound, "Competition does not exist");
+            }
+
+            if (competition.State != Enums.CompetitionState.Open)
+            {
+                throw new ActionException(Enums.ActionResultType.Conflict, "Competition not open for invitations");
+            }
+
+            var participators = new ParticipatorRepository()
+                .GetParticipators(competitionId: competitionId);
+
+            if (participators.SingleOrDefault(participator => 
+                participator.UserId == hostUserId && 
+                participator.Role == Enums.CompetitionRole.Host) == null)
+            {
+                throw new ActionException(Enums.ActionResultType.Conflict, "Only host can invite to competition");
+            }
+
+            var invitations = new CompetitionInvitationRepository().GetInvitations(competitionId: competitionId);
+
+            var users = new UserRepository().GetUsersByName(usernames)
+                .Where(user => 
+                    participators.All(participator => participator.UserId != user.UserId) &&
+                    invitations.All(invitation => invitation.UserId != user.UserId));
+
+            var invitationsToAdd = users.Select(user => new CompetitionInvitation
+            {
+                CompetitionId = competitionId,
+                UserId = user.UserId
+            });
+
+            new CompetitionInvitationRepository().AddInvitations(invitationsToAdd);
+        }
+
+        /// <summary>
+        /// Accept an invitation for user and join user to competition
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="invitationId"></param>
+        public void AcceptInvitation(int userId, int invitationId)
+        {
+            var invitation = new CompetitionInvitationRepository()
+                .GetInvitations(invitationId: invitationId, userId: userId)
+                .SingleOrDefault();
+
+            if (invitation == null)
+            {
+                throw new ActionException(Enums.ActionResultType.Conflict, "Cannot accept invitation of another user");
+            }
+
+            new ParticipatorRepository().AddParticipator(invitation.UserId, invitation.CompetitionId, Enums.CompetitionRole.Participator);
+            new CompetitionInvitationRepository().DeleteInvitation(invitationId);
+        }
+
+        /// <summary>
+        /// Decline an invitation to competition
+        /// </summary>
+        /// <param name="userId"
+        /// <param name="invitationId"></param>
+        public void DeclineInvitation(int userId, int invitationId)
+        {
+            var invitation = new CompetitionInvitationRepository()
+                .GetInvitations(invitationId: invitationId, userId: userId)
+                .SingleOrDefault();
+
+            if (invitation == null)
+            {
+                throw new ActionException(Enums.ActionResultType.Conflict, "Cannot accept invitation of another user");
+            }
+
+            new CompetitionInvitationRepository().DeleteInvitation(invitationId);
+        }
+    }
+}
---
+++ Betkeeper/TestCore/src/Betkeeper/Actions/CompetitionInvitationActionTests.cs
@@ -0,0 +1,377 @@
+﻿using Betkeeper.Actions;
+using Betkeeper.Data;
+using Betkeeper.Enums;
+using Betkeeper.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestTools;
+
+namespace Betkeeper.Test.Betkeeper.Actions
+{
+    [TestFixture]
+    public class CompetitionInvitationActionTests
+    {
+        private BetkeeperDataContext _context;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _context = Tools.GetTestContext();
+            Settings.InitializeOptionsBuilderService(Tools.GetTestOptionsBuilder());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.CompetitionInvitation.RemoveRange(_context.CompetitionInvitation);
+            _context.User.RemoveRange(_context.User);
+            _context.Competition.RemoveRange(_context.Competition);
+            _context.Participator.RemoveRange(_context.Participator);
+            _context.SaveChanges();
+        }
+
+        [Test]
+        public void InviteUsers_CompetitionDoesNotExist_ThrowsActionException()
+        {
+            try
+            {
+                new CompetitionInvitationAction().InviteUsers(1, 2, new List<string> { "testi" });
+                Assert.Fail();
+            }
+            catch (ActionException e)
+            {
+                Assert.AreEqual(ActionResultType.NotFound, e.ActionExceptionType);
+            }
+        }
+
+        [Test]
+        public void InviteUsers_CompetitionNotStarted_ThrowsActionException()
+        {
+            var competitions = new List<Competition>
+            {
+                new Competition
+                {
+                    CompetitionId = 1,
+                    StartTime = DateTime.Now.AddDays(-1)
+                }
+            };
+
+            var users = new List<User>
+            {
+                new User
+                {
+                    Username = "testi"
+                }
+            };
+
+            Tools.CreateTestData(competitions: competitions, users: users);
+
+            try
+            {
+                new CompetitionInvitationAction().InviteUsers(1, 2, new List<string> { "testi" });
+                Assert.Fail();
+            }
+            catch (ActionException e)
+            {
+                Assert.AreEqual(ActionResultType.Conflict, e.ActionExceptionType);
+            }
+        }
+
+        [Test]
+        public void InviteUsers_UserNotCompetitionHost_ThrowsActionException()
+        {
+            var competitions = new List<Competition>
+            {
+                new Competition
+                {
+                    CompetitionId = 1,
+                    StartTime = DateTime.Now.AddDays(-1)
+                }
+            };
+
+            var users = new List<User>
+            {
+                new User
+                {
+                    Username = "testi"
+                }
+            };
+
+            Tools.CreateTestData(competitions: competitions, users: users);
+
+            try
+            {
+                new CompetitionInvitationAction().InviteUsers(1, 2, new List<string> { "testi" });
+                Assert.Fail();
+            }
+            catch (ActionException e)
+            {
+                Assert.AreEqual(ActionResultType.Conflict, e.ActionExceptionType);
+            }
+        }
+
+        [Test]
+        public void InviteUsers_AddsInvitationsForUsersThatExistAndDontHaveAnInvitationAndAreNotInCompetition()
+        {
+            var competitions = new List<Competition>
+            {
+                new Competition
+                {
+                    CompetitionId = 1,
+                    StartTime = DateTime.Now.AddDays(1)
+                }
+            };
+
+            var users = new List<User>
+            {
+                new User
+                {
+                    Username = "testi"
+                },
+                new User
+                {
+                    Username = "IsInCompetition",
+                    UserId = 3
+                },
+                new User
+                {
+                    Username = "IsInvited",
+                    UserId = 4
+                }
+            };
+
+            var participators = new List<Participator>
+            {
+                new Participator
+                {
+                    UserId = 2,
+                    Competition = 1,
+                    Role = CompetitionRole.Host
+                },
+                new Participator
+                {
+                    UserId = 3,
+                    Competition = 1,
+                    Role = CompetitionRole.Participator
+                }
+            };
+
+            var invitations = new List<CompetitionInvitation>
+            {
+                new CompetitionInvitation
+                {
+                    CompetitionId = 1,
+                    UserId = 4
+                }
+            };
+
+            Tools.CreateTestData(
+                competitions: competitions, 
+                users: users, 
+                participators: participators,
+                invitations: invitations);
+
+            new CompetitionInvitationAction().InviteUsers(1, 2, new List<string> { "testi", "notexistingUser", "IsInvited", "IsInCompetition" });
+
+            var results = _context.CompetitionInvitation.ToList();
+            // One already exists
+            Assert.AreEqual(2, results.Count);
+        }
+
+        [Test]
+        public void AcceptInvitation_NotSameUser_ThrowsActionException()
+        {
+            var competitions = new List<Competition>
+            {
+                new Competition
+                {
+                    CompetitionId = 1,
+                    StartTime = DateTime.Now.AddDays(-1)
+                }
+            };
+
+            var users = new List<User>
+            {
+                new User
+                {
+                    Username = "testi",
+                    UserId = 1
+                }
+            };
+
+            var invitations = new List<CompetitionInvitation>
+            {
+                new CompetitionInvitation
+                {
+                    UserId = 1,
+                    CompetitionId = 1,
+                    InvitationId = 1
+                },
+                new CompetitionInvitation
+                {
+                    UserId = 2,
+                    CompetitionId = 1,
+                    InvitationId = 2
+                }
+            };
+
+            Tools.CreateTestData(competitions: competitions, users: users, invitations: invitations);
+
+            try
+            {
+                new CompetitionInvitationAction().AcceptInvitation(2, 1);
+                Assert.Fail();
+            }
+            catch (ActionException e)
+            {
+                Assert.AreEqual(ActionResultType.Conflict, e.ActionExceptionType);
+            }
+        }
+
+        [Test]
+        public void AcceptInvitation_OwnInvitation_Succeeds()
+        {
+            var competitions = new List<Competition>
+            {
+                new Competition
+                {
+                    CompetitionId = 1,
+                    StartTime = DateTime.Now.AddDays(-1)
+                }
+            };
+
+            var users = new List<User>
+            {
+                new User
+                {
+                    Username = "testi",
+                    UserId = 1
+                }
+            };
+
+            var invitations = new List<CompetitionInvitation>
+            {
+                new CompetitionInvitation
+                {
+                    UserId = 1,
+                    CompetitionId = 1,
+                    InvitationId = 1
+                },
+                new CompetitionInvitation
+                {
+                    UserId = 2,
+                    CompetitionId = 1,
+                    InvitationId = 2
+                }
+            };
+
+            Tools.CreateTestData(competitions: competitions, users: users, invitations: invitations);
+
+            new CompetitionInvitationAction().AcceptInvitation(2, 2);
+
+            Assert.AreEqual(1, _context.Participator.ToList().Count, "Participator count");
+            // Check that invitation was deleted
+            Assert.AreEqual(1, _context.CompetitionInvitation.ToList().Count, "Invitation count");
+        }
+
+        [Test]
+        public void DeclineInvitation_NotSameUser_ThrowsActionException()
+        {
+            var competitions = new List<Competition>
+            {
+                new Competition
+                {
+                    CompetitionId = 1,
+                    StartTime = DateTime.Now.AddDays(-1)
+                }
+            };
+
+            var users = new List<User>
+            {
+                new User
+                {
+                    Username = "testi",
+                    UserId = 1
+                }
+            };
+
+            var invitations = new List<CompetitionInvitation>
+            {
+                new CompetitionInvitation
+                {
+                    UserId = 1,
+                    CompetitionId = 1,
+                    InvitationId = 1
+                },
+                new CompetitionInvitation
+                {
+                    UserId = 2,
+                    CompetitionId = 1,
+                    InvitationId = 2
+                }
+            };
+
+            Tools.CreateTestData(competitions: competitions, users: users, invitations: invitations);
+
+            try
+            {
+                new CompetitionInvitationAction().DeclineInvitation(2, 1);
+                Assert.Fail();
+            }
+            catch (ActionException e)
+            {
+                Assert.AreEqual(ActionResultType.Conflict, e.ActionExceptionType);
+            }
+        }
+
+        [Test]
+        public void DeclineInvitation_OwnInvitation_Succeeds()
+        {
+            var competitions = new List<Competition>
+            {
+                new Competition
+                {
+                    CompetitionId = 1,
+                    StartTime = DateTime.Now.AddDays(-1)
+                }
+            };
+
+            var users = new List<User>
+            {
+                new User
+                {
+                    Username = "testi",
+                    UserId = 1
+                }
+            };
+
+            var invitations = new List<CompetitionInvitation>
+            {
+                new CompetitionInvitation
+                {
+                    UserId = 1,
+                    CompetitionId = 1,
+                    InvitationId = 1
+                },
+                new CompetitionInvitation
+                {
+                    UserId = 2,
+                    CompetitionId = 1,
+                    InvitationId = 2
+                }
+            };
+
+            Tools.CreateTestData(competitions: competitions, users: users, invitations: invitations);
+
+            new CompetitionInvitationAction().DeclineInvitation(2, 2);
+
+            // Check that invitation was deleted
+            Assert.AreEqual(1, _context.CompetitionInvitation.ToList().Count, "Invitation count");
+
+            // Check that no participators were created
+            Assert.AreEqual(0, _context.Participator.ToList().Count, "Participator count");
+        }
+    }
+}
