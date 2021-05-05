e5121fa96e0184970d22c5ca2cc466bf35313451
ipuhakka <iiropuhakka@luukku.com>
2021-05-05 19:49:46 +0300

    Add AllowedSelectionCount to Target-table

---
+++ Betkeeper/BetkeeperDb/dbo/Tables/CompetitionInvitation.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[CompetitionInvitation] (
+    [InvitationId] INT IDENTITY (1, 1) NOT NULL,
+    [Competition]  INT NOT NULL,
+    [UserId]       INT NOT NULL,
+    CONSTRAINT [PK_CompetitionInvitation] PRIMARY KEY CLUSTERED ([InvitationId] ASC)
+);
+
